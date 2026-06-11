86bf621eac25754bb8c1c45c2612a427167b4614
jiangxiaowei <jiangxiaowei310@gmail.com>
2026-06-11 10:03:22 +0800

    课表，信息显示

--- 666_zhuanzong/studentinfopage.h
+++ 666_zhuanzong/studentinfopage.h
@@ -14,6 +14,12 @@ class StudentInfoPage : public QWidget
 public:
     explicit StudentInfoPage(QWidget *parent = nullptr);
     ~StudentInfoPage();
+    void setUsername(const QString &username);
+
+private:
+    QString m_username;
+    void loadStudentInfo();
+    void setEditMode(bool editable);
 
 private slots:
     void on_editBtn_clicked();
